3a99f7a7b8748cbfddedf881ded56f6468e3dae3
Devena01 <132521746+Devena01@users.noreply.github.com>
2023-05-04 20:31:30 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+// Human Age in Dog years//
+
+// Step 1: Assigned the value 46 to the 'myAge' variable
+var myAge = 46;
+console.log(myAge);
+
+// Steps 2 and 3: Assigned the value of 2 to the variable 'EarlyYears'
+var earlyYears = 2;
+console.log(earlyYears);
+
+earlyYears *= 10.5;
+console.log(earlyYears);
+
+// Step 4: Subtracted early years (2) from total age (46) to calculate late years
+var DifferenceOfMyAgeAndEarlyYears = 46 - 2;
+console.log(DifferenceOfMyAgeAndEarlyYears);
+
+//Step 5: Multiplied the 'laterYears' variable by 4 to calculate the number of dog years for my human late years
+ var laterYears = 44;
+
+ laterYears *= 4;
+console.log(laterYears);
+
+// Step 6 : console log earlyYears and laterYears: completed above
+
+// Step 7: Added earlyYears and lateYears to calculate total dog years
+
+var MyAgeInDogYears = 21+ 176;
+console.log(MyAgeInDogYears);
+
+//Step 8: Used toLowercase() method to achieve an output with all lower case letters
+
+var myName = 'ANA';
+console.log('ANA'.toLowerCase());
+
+// Step 9: Used string interpolation to change origonal string : My name is NAME. I am Human Age years old in human years which is DOG AGE years old in dog years
+
+var myName = 'Ana';
+var myAge= 46;
+var myAgeInDogYears = 197;
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
